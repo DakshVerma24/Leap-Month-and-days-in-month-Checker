7a42b43c8ef199f6f399375d7777605b50985a5d
DakshVerma24 <daksh.alakhfanboy@gmail.com>
2026-07-15 21:41:12 +0530

    Created main.py
    Made the main file.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+def is_leap(year):
+  return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+year = int(input("Enter Year: "))
+if is_leap(year):
+  print("Leap")
+else:
+  print("Not Leap")
+  
